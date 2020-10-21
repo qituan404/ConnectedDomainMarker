6a6f19f1f40fa6e367cb2f9a7e79e659bc421fd7
SRH-Xavier <71642196+SRH-Xavier@users.noreply.github.com>
2020-10-21 09:32:57 +0800

    连通域标记

---
+++ ConnectedDomainMarker/ConnectedDomainMarker.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+int main()
+{
+	Mat binaryMat, labelMat, stateMat, centerMat, resultMat;
+	Mat srcMat = imread("0.jpg", 0);
+	threshold(srcMat, binaryMat, 100, 255, THRESH_BINARY);		//二值化算法
+	int Number = connectedComponentsWithStats(binaryMat, labelMat, stateMat, centerMat, 8, CV_32S);	//连通域标记函数
+
+	cout << "硬币数= " << Number - 1 << endl;  //硬币数为统计值减去一张背景的连通域
+
+	for (int i = 1; i < Number; i++)
+	{
+		Rect bbx;
+		bbx.x = stateMat.at<int>(i, 0);
+		bbx.y = stateMat.at<int>(i, 1);
+		bbx.width = stateMat.at<int>(i, 2);
+		bbx.height = stateMat.at<int>(i, 3);
+		rectangle(binaryMat, bbx, CV_RGB(255, 255, 255), 1, 8, 0);
+	}
+
+	imshow("frame", srcMat);
+	imshow("binaryMat", binaryMat);
+	waitKey(0);
+	return 0;
+}
